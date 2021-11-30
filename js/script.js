94fd468237eaa4a37c0da3bdb382a729355e2110
VincenzoPiparo <v.pipar.97@gmai.com>
2021-11-30 15:11:18 +0100

    array

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Visualizzare con degli alert 5 numeri casuali. Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+// 1 [] impostare un array e stamparlo tramite un allert, con all'interno 5 numeri
+const fiveNumberArray = [7, 13, 27, 58, 66]
+
+// 2 [] far partire un timer di 30 secondi
+
+
+// 3 [] allo scadere dei 30 secondi creiamo 5 prompt() dove l'utente inserirà i 5 numeri visti in precedenza
+
+
+// 4 [] 
