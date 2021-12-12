80912fce180f30efc5358422054afec31adc6ec5
VincenzoPiparo <v.pipar.97@gmai.com>
2021-12-12 16:03:55 +0100

    CREATION TIMER

--- js/script.js
+++ js/script.js
@@ -2,13 +2,21 @@
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
-// 1 [] impostare un array e stamparlo tramite un allert, con all'interno 5 numeri
-const fiveNumberArray = [7, 13, 27, 58, 66]
 
-// 2 [] far partire un timer di 30 secondi
+// Visualizzare con degli alert 5 numeri casuali.
+
+// Creare un timer di 30 secondi mediante il ciclo WHILE
+let i = 0;
+const randomNumber = [];
+while(i < 5) {
+    let randomNum = getRndInteger(1, 100);
+    alert(randomNum);
+    randomNumber.push(randomNum);
+    i++;
+}
+console.log(randomNumber);
 
 
-// 3 [] allo scadere dei 30 secondi creiamo 5 prompt() dove l'utente inserirà i 5 numeri visti in precedenza
+setTimeout(userInput, 30000);
 
-
-// 4 [] 
+// Facciamo inserire i numeri all'utente 1 alla volta
